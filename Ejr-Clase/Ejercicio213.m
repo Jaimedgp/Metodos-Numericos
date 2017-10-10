6dbad1cc1818038ce0cc05f9bedf8941a1ddb728
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2017-10-10 11:32:14 +0200

    Upgrade

--- Ejr-Clase/Ejercicio213.m
+++ Ejr-Clase/Ejercicio213.m
@@ -9,6 +9,8 @@ close all;
 x = -1:1/5:1; % se crea el vector con los puntos -1, -8/10, -6/10, ..., 8/10, 1
 fx = 1./ (1+25.*x.^2); % se evalua la funcion en los puntos
 
+xTh = linspace(-1,1, 200); % vector para los puntos teoricos
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%   AJUSTE POLINOMIAL   %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -17,7 +19,6 @@ p = polyfit(x, fx, 10); % se obtiene el polinomio del ajuste polinomial
 
 % se crean los vectores con los datos teoricos obtenidos del polinomio del
 % ajuste
-xTh = linspace(-1,1);
 yTh = polyval(p, xTh);
 
 % Se representan ambos datos
@@ -25,12 +26,24 @@ hold on
     plot(x, fx, 'ro'); % Datos experimentales
     plot(xTh, yTh, 'b'); % Datos teoricos del ajuste
     plot(xTh, 1./ (1+25.*xTh.^2), 'g') % La funcion de verdad
+    title('Interpolacion polinomial')
+    legend('Experimentales', 'Interpolacion', 'Funcion real')
 hold off
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%   INTERPOLACION SPLINER   %%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%      SPLINES CUBICOS   %%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-disp('Hola')
+yTh = spline(x, fx, xTh); % Interpolacion por splines cubicos
 
+% Se representan ambos datos
+hold on 
+    plot(x, fx, 'ro'); % Datos experimentales
+    plot(xTh, yTh, 'b'); % Datos teoricos del ajuste
+    plot(xTh, 1./ (1+25.*xTh.^2), 'g') % La funcion de verdad
+    title('Splines cubicos')
+    legend('Experimentales', 'Splines', 'Funcion real')
+hold off
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 
